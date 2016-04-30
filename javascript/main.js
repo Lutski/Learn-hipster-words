70e5f204039d0112a582aa69491373b97acb116e
LudvigSoerensen <ludvigsoerensen@gmail.com>
2016-04-30 15:58:19 +0200

    Added hipster-words.js and functionality

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,40 @@
 $(document).ready(function() {
-  console.log("Hello world!");
+  getRandomWord();
+  newWord();
 });
+
+function getRandomWord() {
+  var allPossibilities = hiptionary.length
+  var randomNumber = Math.floor((Math.random() * allPossibilities) + 1)
+
+  var word = hiptionary[randomNumber]["name"];
+  var meaning = hiptionary[randomNumber]["meaning"];
+  var example = hiptionary[randomNumber]["example"];
+
+  $(".word").text(word);
+  $(".word-meaning").text(meaning);
+}
+
+
+function mustacheSlideUpDown() {
+  $(".mustache-wrapper").addClass("mustache-up-down-ani");
+
+  $(".mustache-up-down-ani").on('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+  function(e) {
+    // code to execute after animation ends
+    $(".mustache-wrapper").removeClass("mustache-up-down-ani");
+  });
+
+}
+
+
+function newWord() {
+  $(".new-word-btn").click(function() {
+    mustacheSlideUpDown();
+
+    // Runs getRandomWord() after 1 second (delays it)
+    setTimeout(function() {
+      getRandomWord();
+    }, 1000);
+  });
+}
