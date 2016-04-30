f94a9f72d48a45359b235aee237875bd117e1271
LudvigSoerensen <ludvigsoerensen@gmail.com>
2016-04-30 20:28:27 +0200

    Animation dropdown picker is working

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   openAnimationPicker();
-
+  getMustacheAnimationNameFromDropdown();
   getRandomWord();
   newWord();
 });
+
+// Global variable that determines the mustache animation
+var animation = "mustache-up-down"
+
+
 
 function getRandomWord() {
   var allPossibilities = hiptionary.length
@@ -17,22 +22,47 @@ function getRandomWord() {
   $(".word-meaning").text(meaning);
 }
 
+function getMustacheAnimation() {
+  $(".mustache-wrapper").addClass(animation + "-ani");
 
-function mustacheSlideUpDown() {
-  $(".mustache-wrapper").addClass("mustache-up-down-ani");
-
-  $(".mustache-up-down-ani").on('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+  $("." + animation + "-ani").on('webkitAnimationEnd oanimationend msAnimationEnd animationend',
   function(e) {
     // code to execute after animation ends
-    $(".mustache-wrapper").removeClass("mustache-up-down-ani");
+    $(".mustache-wrapper").removeClass(animation + "-ani");
   });
+}
 
+function getMustacheAnimationWithinMustache() {
+  $(".mustache-wrapper .mustache").addClass(animation + "-ani");
+
+  $("." + animation + "-ani").on('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+  function(e) {
+    // code to execute after animation ends
+    $(".mustache-wrapper .mustache").removeClass(animation + "-ani");
+  });
+}
+
+function getMustacheAnimationNameFromDropdown() {
+  $('.animations li').click(function() {
+    content = $(this).html();
+
+    // Set global variable 'animation' to new animation
+    animation = content
+
+  });
 }
 
 
 function newWord() {
   $(".new-word-btn").click(function() {
-    mustacheSlideUpDown();
+
+    mustacheAloneAnimations = ["mustache-expand", "mustache-kick"];
+
+    if (animation == "mustache-expand" || animation == "mustache-kick") {
+      getMustacheAnimationWithinMustache();
+    } else {
+      getMustacheAnimation();
+    }
 
     // Runs getRandomWord() after 1 second (delays it)
     setTimeout(function() {
